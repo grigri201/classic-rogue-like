09b724b93f9b559545b70f7817a2d9e849f0bf7d
grigri201 <grigri201@outlook.com>
2024-01-01 10:18:33 +0800

    fix slime splitting error

--- entity/character/enemy/slime/slime.gd
+++ entity/character/enemy/slime/slime.gd
@@ -10,17 +10,17 @@ func initialize():
   _enemy_spawner = get_tree().current_scene.get_node("%EnemySpawner")
 
 func _on_skill_used():
-  var target_cell = _check_cells()
+  var target_cell = get_valid_cell()
   if target_cell != Vector2i.ZERO:
     _split_slime(target_cell)
 
-func _check_cells():
+func get_valid_cell():
   var directions = [Vector2i.UP, Vector2i.DOWN, Vector2i.LEFT, Vector2i.RIGHT, Vector2i(-1, -1), Vector2i(-1, 1), Vector2i(1, -1), Vector2i(1, 1)]
   for direction in directions:
     var target_cell = cell + direction
     var block_move = _map_manager.cell_intersect_point(target_cell, MapEnum.PhysicsLayer().BLOCK_MOVEMENT, true, [])
     var pickable_item = _map_manager.cell_intersect_point(target_cell, MapEnum.PhysicsLayer().PICKABLE_OBJECT, true, [])
-    if block_move.size() == 0 or pickable_item.size() == 0:
+    if block_move.size() == 0 and pickable_item.size() == 0:
       return target_cell
   return Vector2i.ZERO
   
